5fa56a21215dad9d6d6d5922b22beb63d6f9cdbe
Nikhil Kumar Yadav <151929628+Nikhil020Yadav@users.noreply.github.com>
2024-07-20 08:21:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,17 +10,18 @@ const {app,server}=require ('./socket/index')
 
 //const app=express()
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL); 
+    res.header('Access-Control-Allow-Origin', 'https://vartalap-frontend-qet43xgsv-nikhil020yadavs-projects.vercel.app'); 
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
 const cookiesParser=require('cookie-parser')
-// app.use(cors({
-//     origin:process.env.FRONTEND_URL,
-//     credentials:true
-// }))
-app.use(cors());
+app.use(cors({
+    //origin:process.env.FRONTEND_URL,
+    origin: 'https://vartalap-frontend-qet43xgsv-nikhil020yadavs-projects.vercel.app',
+    credentials:true
+}))
+// app.use(cors());
 
 app.use(express.json())
 app.use(cookiesParser())
